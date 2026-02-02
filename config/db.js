6fd7c5f7b441eea0426e5f9dc11d8c3893841345
ccn-platform <ibracredity@gmail.com>
2026-02-02 10:01:17 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-
-    const mongoURI =
+      const mongoURI =
       process.env.MONGO_URI_ATLAS ||
       "mongodb+srv://app_user:Ibra987654321@commodity-credit-cluster0.jffgeq2.mongodb.net/ccn_database";
 
+     
     console.log("Mongo URI used:", mongoURI);
 
     await mongoose.connect(mongoURI, {
