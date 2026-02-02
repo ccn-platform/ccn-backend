6e71976b1ee3f68916ca8a344d7d5893d0924a16
ccn-platform <ibracredity@gmail.com>
2026-02-02 09:51:45 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,20 +3,19 @@
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-const mongoURI =
-  process.env.MONGO_URI_ATLAS ||
-  process.env.MONGO_URI ||
-  "mongodb+srv://app_user:Ibra987654321@commodity-credit-cluster0.jffgeq2.mongodb.net/ccn_database";
 
-    
+    const mongoURI =
+      process.env.MONGO_URI_ATLAS ||
+      "mongodb+srv://app_user:Ibra987654321@commodity-credit-cluster0.jffgeq2.mongodb.net/ccn_database";
+
+    console.log("Mongo URI used:", mongoURI);
+
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log(
-      `✅ MongoDB Connected Successfully (${process.env.NODE_ENV})`
-    );
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
