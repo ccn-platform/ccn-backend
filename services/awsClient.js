44d75ab923e568e6a8939db2ddc907da5ed00232
ccn-platform <ibracredity@gmail.com>
2026-02-07 00:57:27 -0800

    Create awsClient.js

---
+++ services/awsClient.js
@@ -0,0 +1,11 @@
+const { RekognitionClient } = require("@aws-sdk/client-rekognition");
+
+const client = new RekognitionClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+module.exports = client;
