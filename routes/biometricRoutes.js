6123a39ec96bf0f8097b2151ddaa0425689102e8
ccn-platform <ibracredity@gmail.com>
2026-02-07 01:03:08 -0800

    Update biometricRoutes.js

--- routes/biometricRoutes.js
+++ routes/biometricRoutes.js
@@ -1,4 +1,4 @@
- const express = require("express");
+  const express = require("express");
 const router = express.Router();
 const biometricController = require("../controllers/biometricController");
 const rateLimiter = require("../middleware/rateLimiter");
