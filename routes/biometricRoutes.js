081e85aaf92822059d2a23a2e419dafec7ba76ed
ccn-platform <ibracredity@gmail.com>
2026-02-16 00:54:22 -0800

    Update biometricRoutes.js

--- routes/biometricRoutes.js
+++ routes/biometricRoutes.js
@@ -2,10 +2,12 @@
 const router = express.Router();
 const biometricController = require("../controllers/biometricController");
 const rateLimiter = require("../middleware/rateLimiter");
+const registerGuard = require("../middleware/registerGuard");
 
 router.post(
   "/verify",
   rateLimiter.biometric,
+   registerGuard,   // 🔴 HAPA NDIO INAWEKA device guard
   biometricController.verifyCustomerFace   // ✅ SAHIHI
 );
 
