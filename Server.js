dc284898194c65d31d479e516cb076c0656b0aca
ccn-platform <ibracredity@gmail.com>
2026-02-16 00:48:49 -0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -5,6 +5,8 @@ const cronScheduler = require("./automation/cronScheduler");
 
 // 🔴 IMPORT AWS COLLECTION INIT
 const { ensureCollection } = require("./services/biometricService");
+// 🔴 IMPORT CLEANUP JOB
+const cleanupBiometrics = require("./jobs/cleanupBiometric");
 
 // 1️⃣ CONNECT DB
 connectDB();
@@ -15,9 +17,16 @@ ensureCollection();
 // 3️⃣ START AUTOMATION JOBS
 cronScheduler.init();
 
+// 4️⃣ CLEANUP EXPIRED BIOMETRICS (MUHIMU)
+setInterval(() => {
+  cleanupBiometrics();
+}, 5 * 60 * 1000); // kila dakika 5
+
+
 // 4️⃣ START API SERVER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
