a16f566c102c89a426cabaecad275db27e2213cc
ccn-platform <ibracredity@gmail.com>
2026-02-16 22:08:03 -0800

    Update awsClient.js

--- services/awsClient.js
+++ services/awsClient.js
@@ -1,5 +1,4 @@
   const { RekognitionClient } = require("@aws-sdk/client-rekognition");
-const { NodeHttpHandler } = require("@aws-sdk/node-http-handler");
 
 const client = new RekognitionClient({
   region: process.env.AWS_REGION,
@@ -7,10 +6,6 @@ const client = new RekognitionClient({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
   },
-  requestHandler: new NodeHttpHandler({
-    connectionTimeout: 5000, // 5 sec connect
-    socketTimeout: 8000,     // 8 sec max wait
-  }),
 });
 
 module.exports = client;
