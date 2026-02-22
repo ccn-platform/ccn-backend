d90939b1134f94e1c46890bb9991f64978f48187
ccn-platform <ibracredity@gmail.com>
2026-02-22 00:24:21 -0800

    Update loanController.js

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -46,7 +46,11 @@ class LoanController {
 
       return res.json(result);
     } catch (err) {
-      return res.status(400).json({ error: err.message });
+
+      return res.status(400).json({
+       success: false,
+     message: err.message
+  });
     }
   }
 
