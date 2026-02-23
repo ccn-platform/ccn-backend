ced60a974d1bde985ff17e796b826d7a5e23a251
ccn-platform <ibracredity@gmail.com>
2026-02-23 10:55:12 -0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
- require("dotenv").config();
+require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 const cronScheduler = require("./automation/cronScheduler");
@@ -35,3 +35,4 @@ app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
  
+
