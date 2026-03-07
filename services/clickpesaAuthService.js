5af41cb8aa7d3ffafd4c958ccf3540cf926b819b
ccn-platform <ibracredity@gmail.com>
2026-03-06 23:28:04 -0800

    Update clickpesaAuthService.js

--- services/clickpesaAuthService.js
+++ services/clickpesaAuthService.js
@@ -4,24 +4,18 @@ class ClickPesaAuthService {
 
  async getToken() {
 
-  const url =
-   `${process.env.CLICKPESA_BASE_URL}/third-parties/generate-token`;
-
   const response = await axios.post(
-   url,
+   `${process.env.CLICKPESA_BASE_URL}/third-parties/generate-token`,
    {},
    {
     headers: {
      "api-key": process.env.CLICKPESA_API_KEY,
      "client-id": process.env.CLICKPESA_CLIENT_ID
-    },
-    timeout: 10000
+    }
    }
   );
 
-  if (!response.data || !response.data.token) {
-   throw new Error("Token not returned from ClickPesa");
-  }
+  console.log("CLICKPESA TOKEN RESPONSE:", response.data);
 
   return response.data.token;
  }
