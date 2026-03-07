5d732af7e41c335d21797b223af176804d4273e4
ccn-platform <ibracredity@gmail.com>
2026-03-06 18:25:54 -0800

    Update generateReference.js

--- utils/generateReference.js
+++ utils/generateReference.js
@@ -1,24 +1,33 @@
- // utils/generateReference.js
+const crypto = require("crypto");
+
+function randomCode(length = 10) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const bytes = crypto.randomBytes(length);
+  let code = "";
+
+  for (let i = 0; i < length; i++) {
+    code += chars[bytes[i] % chars.length];
+  }
+
+  return code;
+}
+
+function controlNumber() {
+  return "CN-" + randomCode(12);
+}
+
+function loanReference() {
+  return "LN-" + randomCode(10);
+}
+
+function transactionId() {
+  const time = Date.now().toString(36).toUpperCase();
+  return `TX-${time}-${randomCode(8)}`;
+}
 
 module.exports = {
-  randomCode(length = 10) {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let code = "";
-    for (let i = 0; i < length; i++) {
-      code += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return code;
-  },
-
-  controlNumber() {
-    return "CN-" + this.randomCode(12);
-  },
-
-  loanReference() {
-    return "LN-" + this.randomCode(10);
-  },
-
-  transactionId() {
-    return "TX-" + this.randomCode(15);
-  }
-};
+  randomCode,
+  controlNumber,
+  loanReference,
+  transactionId
+}; 
