a535652ac6b4d54e73d199f645ed61ad74efbee7
ccn-platform <ibracredity@gmail.com>
2026-03-06 21:58:52 -0800

    Update clickpesaAuthService.js

--- services/clickpesaAuthService.js
+++ services/clickpesaAuthService.js
@@ -1,22 +1,38 @@
-const axios = require("axios");
+ const axios = require("axios");
 
 class ClickPesaAuthService {
 
  async getToken() {
 
-  const response = await axios.post(
-   `${process.env.CLICKPESA_BASE_URL}/oauth/token`,
-   {},
-   {
-    headers: {
-     "X-CLIENT-ID": process.env.CLICKPESA_CLIENT_ID,
-     "X-API-KEY": process.env.CLICKPESA_API_KEY,
-     "Content-Type": "application/json"
-    }
-   }
-  );
+  try {
 
-  return response.data.access_token;
+   const response = await axios.post(
+    `${process.env.CLICKPESA_BASE_URL}/oauth/token`,
+    {},
+    {
+     headers: {
+      "X-CLIENT-ID": process.env.CLICKPESA_CLIENT_ID,
+      "X-API-KEY": process.env.CLICKPESA_API_KEY,
+      "Content-Type": "application/json"
+     },
+     timeout: 10000
+    }
+   );
+
+   if (!response.data?.access_token) {
+    throw new Error("Token not returned from ClickPesa");
+   }
+
+   return response.data.access_token;
+
+  } catch (error) {
+
+   console.error("ClickPesa token error:", 
+    error.response?.data || error.message
+   );
+
+   throw new Error("Failed to generate ClickPesa token");
+  }
 
  }
 
